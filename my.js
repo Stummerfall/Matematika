cd0003f2a708b315afe6fe0c4f8e091acb68f054
Stummerfall <46425852+Stummerfall@users.noreply.github.com>
2019-01-08 00:40:52 +0300

    Add files via upload

---
+++ my.js
@@ -0,0 +1,18 @@
+/*function viewDiv(){
+	if (document.getElementById("div1").style.display == "none")
+	{document.getElementById("div1").style.display = "block";}
+	else document.getElementById("div1").style.display = "none";
+};*/
+
+function proiz(){
+	window.open("proizvod.html","_self")
+}
+function integr(){
+	window.open("proizvod.html","_self")
+}
+function matr(){
+	window.open("proizvod.html","_self")
+}
+function vect(){
+	window.open("proizvod.html","_self")
+}

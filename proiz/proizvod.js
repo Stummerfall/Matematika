16ba474c619b0c779f4440cd3e9a5245bf06bf6c
Stummerfall <46425852+Stummerfall@users.noreply.github.com>
2019-01-08 06:11:17 +0300

    Add files via upload

--- proiz/proizvod.js
+++ proiz/proizvod.js
@@ -1,1 +1,21 @@
-// JavaScript Document
+function pon(){
+	window.open("pon.html","_self")
+}
+function glav(){
+	window.open("../index.html","_self")
+}
+function svoistva(){
+	window.open("svoistva.html","_self")
+}
+function tab(){
+	window.open("tab.html","_self")
+}
+function opr(){
+	window.open("opr.html","_self")
+}
+function priz(){
+	window.open("priz.html","_self")
+}
+function smisl(){
+	window.open("smisl.html","_self")
+}
